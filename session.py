65c644186d3cfbadabc462c517603ec4ee3ea899
Philip Kranz <philip.kranz@googlemail.com>
2011-08-15 09:43:58 +0200

    Some cleanup

--- session.py
+++ session.py
@@ -6,32 +6,30 @@ class IOStream:
     def __init__(self):
         self.lock = threading.Condition()
         self.out = ""
-        self.backlog = ""
+
+    def has_data(self):
+        return self.out == ""
 
     def read(self, blocking=False):
         self.lock.acquire();
         if self.out == "" and blocking:
             self.lock.wait()
-        tmp = self.out;
+        ret = self.out;
         self.out = ""
         self.lock.release();
 
-        return tmp;
-
-    def has_data(self):
-        return self.out == ""
+        return ret;
 
     def write(self, data):
         self.lock.acquire()
         self.out += data
-        self.backlog += data
         self.lock.notify_all();
         self.lock.release()
 
     def writeln(self, data):
         self.write(data + "\n")
 
-class CbType:
+class Event:
     STDIO       = 1
     ERROR       = 2
     CONNECTED   = 3
@@ -39,16 +37,10 @@ class CbType:
 
 class Session:
     NEWLINE = "\n";
-    connected = False
-    mode = 0
-    properties = {}
-    callback = None
-    fresh_data = ""
-    completer = None
 
-    def __init__(self, host, port):
-        self.input_thread = threading.Thread(None, self.input_run)
-        self.output_thread = threading.Thread(None, self.output_run)
+    def __init__(self, host, port, callback=None):
+        self.input_thread = threading.Thread(None, self._input_run)
+        self.output_thread = threading.Thread(None, self._output_run)
         self.input_thread.daemon = True
         self.output_thread.daemon = True
         self.host, self.port = host, port
@@ -57,46 +49,55 @@ class Session:
         self.stdin = IOStream()
         self.completer = Completer()
 
-    def set_callback(self, callback):
+        self.connected = False
+        self.mode = 0
+        self.properties = {}
         self.callback = callback
-    
-    def do_callback(self, typ, arg=None):
-        if self.callback:
-            self.callback(self, typ, arg)
+        self.fresh_data = ""
 
     def connect(self):
         try:
             self.telnet = telnetlib.Telnet(self.host, self.port)
         except Exception, msg:
             self.stderr.writeln("Could not connect: %s." % msg)
-            self.do_callback(CbType.ERROR)
-            self.do_callback(CbType.CLOSED)
+            self._do_callback(Event.ERROR)
+            self._do_callback(Event.CLOSED)
             return
 
         self.connected = True
-        self.do_callback(CbType.CONNECTED)
+        self._do_callback(Event.CONNECTED)
 
         self.input_thread.start()
         self.output_thread.start()
 
     def close(self):
         self.telnet.close()
-        self.do_callback(CbType.CLOSED)
+        self._do_callback(Event.CLOSED)
 
-    def input_run(self):
+    def current_line(self):
+        return self.fresh_data.split(self.NEWLINE)[-1]
+
+    def _do_callback(self, typ, arg=None):
+        if self.callback:
+            self.callback(self, typ, arg)
+
+    def _input_run(self):
         while self.connected:
             try:
                 data = self.telnet.read_very_eager()
             except EOFError, e:
                 self.connected = False
-                self.do_callback(CbType.CLOSED)
+                self._do_callback(Event.CLOSED)
                 break
             except:
                 break
 
             if data == "":
                 continue
+            
+            data = data.replace("\r\n", self.NEWLINE)
 
+            # Process complete lines
             self.fresh_data += data
             if self.NEWLINE in self.fresh_data:
                 lines = self.fresh_data.split(self.NEWLINE)
@@ -106,33 +107,33 @@ class Session:
                     self.fresh_data = lines[-1]
                     lines = lines[:-1]
                 for l in lines:
-                    self.process_line(l)
+                    self._process_line(l)
 
+            # Write to output stream
             if not self.mode in self.out:
                 self.out[self.mode] = IOStream()
             self.out[self.mode].write(data)
-            self.do_callback(CbType.STDIO, self.mode)
+            self._do_callback(Event.STDIO, self.mode)
     
-    def output_run(self):
+    def _output_run(self):
         while self.connected:
             data = self.stdin.read(True)
             try:
                 self.telnet.write(data)
             except IOError, e:
                 self.stderr.writeln("Connection closed.")
-                self.do_callback(CbType.ERROR)
+                self._do_callback(Event.ERROR)
                 self.connected = False
 
-    def current_line(self):
-        return self.fresh_data.split(self.NEWLINE)[-1]
-
-    def process_line(self, line):
+    def _process_line(self, line):
         self.completer.parse_line(line)
 
 class Completer:
     nouns = set()
 
     def complete(self, text, state):
+        if text == "":
+            return None
         lastword = text.split()[-1]
         for n in self.nouns:
             if n.startswith(lastword) and n != lastword:
