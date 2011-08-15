6e915fbdc06876042945d21cf4aebc9c9b26ac4c
Philip Kranz <philip.kranz@googlemail.com>
2011-08-15 09:41:20 +0200

    Improved command dispatch in Serial

--- interfaces/serial.py
+++ interfaces/serial.py
@@ -1,6 +1,9 @@
-from session import *
 import sys
 import readline
+
+from session import Session, Event
+
+VERSION = "0.1"
 
 class Serial:
     def __init__(self):
@@ -20,6 +23,11 @@ class Serial:
             return self.sessions[self.sname]
 
     def run(self):
+        global options
+
+        print "mudblood serial interface version %s" % VERSION
+        print "type '%shelp' for a list of commands." % options.prefix
+
         readline.parse_and_bind("tab: complete")
 
         while True:
@@ -29,40 +37,16 @@ class Serial:
                 break
 
             words = line.split()
-            if len(words) == 0:
-                words = [""]
 
-            if words[0] == ".session":
-                if len(words) == 2:
-                    try:
-                        self.switch_session(words[1])
-                    except KeyError, e:
-                        self.error("No such session");
-                elif len(words) < 4:
-                    self.message("Syntax: .session <name> <host> <port>")
-                    self.message("    or: .session <name>")
-                else:
-                    if words[1] in self.sessions.keys():
-                        self.error("There is already a session named '%s'" % words[1])
-                    else:
-                        self.sessions[words[1]] = Session(words[2], int(words[3]))
-                        self.switch_session(words[1])
-
-                        self.sessions[words[1]].set_callback(self.session_cb)
-                        self.sessions[words[1]].connect()
-
-            elif words[0] == ".sessions":
-                if len(self.sessions) == 0:
-                    self.message("No sessions")
-                else:
-                    for s in self.sessions.keys():
-                        self.message(s)
-
-            elif words[0] == ".quit":
-                for s in self.sessions.values():
-                    s.close()
-                break
-
+            if len(words) > 0 and words[0][0] == options.prefix:
+                args = words[1:]
+                try:
+                    getattr(self, "cmd_%s" % words[0][1:])(*args)
+                except TypeError:
+                    self.error("Syntax error")
+                    self.cmd_help(words[0][1:])
+                except AttributeError:
+                    self.error("Unknown command")
             else:
                 if self.session() == None:
                     self.error("Not connected")
@@ -72,21 +56,20 @@ class Serial:
         self.message("Bye!")
 
     def session_cb(self, ob, typ, arg):
-        # O(n) name lookup... ugly!
         name = ""
         for s in self.sessions.keys():
             if self.sessions[s] == ob:
                 name = s
                 break
 
-        if ob == self.session() and typ == CbType.STDIO:
+        if ob == self.session() and typ == Event.STDIO:
             sys.stdout.write(ob.out[arg].read())
             sys.stdout.flush()
-        elif typ == CbType.ERROR:
+        elif typ == Event.ERROR:
             self.error(ob.stderr.read())
-        elif typ == CbType.CONNECTED:
+        elif typ == Event.CONNECTED:
             self.message("[%s] Session connected" % name)
-        elif typ == CbType.CLOSED:
+        elif typ == Event.CLOSED:
             self.message("[%s] Session closed" % name)
             del self.sessions[name]
             if len(self.sessions) > 0:
@@ -104,3 +87,62 @@ class Serial:
             for i in self.sessions[newsession].out:
                 sys.stdout.write(i.read())
 
+    # commands ---
+
+    def cmd_session(self, name, host="", port=0):
+        """.session <name> [<host> <port>]
+
+           Switch session or create new session."""
+
+        if host == "" and port == 0:
+            try:
+                self.switch_session(name)
+            except KeyError, e:
+                self.error("No such session");
+        else:
+            if name in self.sessions.keys():
+                self.error("There is already a session named '%s'" % name)
+            else:
+                self.sessions[name] = Session(host, int(port), self.session_cb)
+                self.switch_session(name)
+
+                self.sessions[name].connect()
+
+    def cmd_sessions(self):
+        """.sessions
+
+           List all active sessions."""
+
+        if len(self.sessions) == 0:
+            self.message("No sessions")
+        else:
+            for s in self.sessions.keys():
+                self.message(s)
+
+    def cmd_help(self, topic="commands"):
+        """.help [<command>]
+
+           Show help for command"""
+
+        if topic == "commands":
+            self.message("Commands:")
+            for c in dir(self):
+                if c.startswith("cmd_"):
+                    self.message("\t" + c[4:])
+            return
+        try:
+            self.message("\n".join(map(lambda x: x.strip(),
+                                       getattr(self, "cmd_%s" % topic).__doc__.split("\n")))
+                             .replace("." + topic, options.prefix + topic))
+        except AttributeError:
+            self.error("Topic not found")
+
+    def cmd_quit(self):
+        """.quit
+
+           Quit."""
+
+        for s in self.sessions.values():
+            s.close()
+        self.message("Bye!")
+        sys.exit(0)
