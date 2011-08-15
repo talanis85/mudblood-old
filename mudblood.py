1357ccf2c7d10f4edd7d82db6efaea62969ab9c3
Philip Kranz <philip.kranz@googlemail.com>
2011-08-15 09:42:38 +0200

    Support serial and curses. Command prefix can be changed.

--- mudblood.py
+++ mudblood.py
@@ -1,4 +1,52 @@
-import interfaces.serial
+#!/usr/bin/env python
 
-s = interfaces.serial.Serial()
-s.run()
+# mudblood - A mud client
+#
+# Copyright (c) 2011 Philip Kranz
+
+from __future__ import absolute_import
+
+import sys
+import interfaces.serial, interfaces.curses
+
+from optparse import OptionParser
+
+VERSION = "0.1"
+
+def main():
+    parser = OptionParser()
+
+    parser.add_option("-i", "--interface",
+                      action =  "store",
+                      dest =    "interface",
+                      type =    "choice",
+                      choices = ["serial", "curses"],
+                      default = "serial",
+                      help =    "Choices: serial (default) or curses",)
+
+    parser.add_option("-p", "--prefix",
+                      action =  "store",
+                      dest =    "prefix",
+                      type =    "string",
+                      default = ".",
+                      help =    "Command Prefix (default: '.')")
+
+
+    (options, args) = parser.parse_args()
+
+
+    if options.interface == "serial":
+        interfaces.serial.options = options
+        s = interfaces.serial.Serial()
+        s.run()
+
+    elif options.interface == "curses":
+        interfaces.curses.options = options
+        s = interfaces.curses.Curses()
+        s.run()
+
+    else:
+        print "Interface not known"
+        sys.exit(1)
+
+main()
